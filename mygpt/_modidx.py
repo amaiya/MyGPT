996ff9c7eedccebfee81079dd6f5f572e0abd756
Arun S. Maiya <arun@maiya.net>
2023-02-24 09:02:34 -0500

    test commit

--- mygpt/_modidx.py
+++ mygpt/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://amaiya.github.io',
                 'git_url': 'https://github.com/amaiya/mygpt',
                 'lib_path': 'mygpt'},
-  'syms': {'mygpt.core': {'mygpt.core.foo': ('core.html#foo', 'mygpt/core.py')}}}
+  'syms': { 'mygpt.core': { 'mygpt.core.foo': ('core.html#foo', 'mygpt/core.py'),
+                            'mygpt.core.say_hello': ('core.html#say_hello', 'mygpt/core.py')}}}
